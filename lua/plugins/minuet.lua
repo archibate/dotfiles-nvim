be9ff57cef3e8ae5b33ac51dfa77f543bc7265c4
archibate <1931127624@qq.com>
2026-02-20 13:48:49 +0800

    use ollama for minuet

--- lua/plugins/minuet.lua
+++ lua/plugins/minuet.lua
@@ -9,7 +9,7 @@ return {
             },
             keymap = {
                 -- accept whole completion
-                accept = '<A-A>',
+                accept = '<A-e>',
                 -- accept one line
                 accept_line = '<A-a>',
                 -- accept n lines (prompts for number)
@@ -19,19 +19,30 @@ return {
                 prev = '<A-[>',
                 -- Cycle to next completion item, or manually invoke completion
                 next = '<A-]>',
-                dismiss = '<A-e>',
+                dismiss = '<A-x>',
             },
         },
+        n_completions = 3,
+        context_window = 4096,
         provider = 'openai_fim_compatible',
         provider_options = {
             openai_fim_compatible = {
-                api_key = 'DEEPSEEK_API_KEY',
-                name = 'deepseek',
+                api_key = 'TERM',
+                name = 'Ollama',
+                end_point = 'http://localhost:11434/v1/completions',
+                model = 'qwen2.5-coder:14b',
                 optional = {
-                    max_tokens = 512,
-                    temperature = 0.9,
-                    top_k = 10,
+                    max_tokens = 128,
+                    temperature = 0.4,
+                    top_p = 0.96,
                 },
+                -- api_key = 'DEEPSEEK_API_KEY',
+                -- name = 'deepseek',
+                -- optional = {
+                --     max_tokens = 512,
+                --     temperature = 0.9,
+                --     top_p = 0.96,
+                -- },
             },
         },
     },
